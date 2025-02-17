e38efd0862dbaa019601a3be1c38e8f24c146665
Jair <jair.guerreroaq@outlook.com>
2025-02-17 00:48:28 -0600

    Add loop

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = 0;
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+  console.log(numeroUsuario);
+  /*
+    Este codigo realiza
+    la comparacion
+    */
+  if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condicion
+    alert(`Acertaste, el numero es: ${numeroUsuario}`);
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      alert("El numero secreto es mayor");
+    }
+    //La condicion no se cumplio
+    //alert('Lo siento, no acertaste el numero');
+  }
 }
