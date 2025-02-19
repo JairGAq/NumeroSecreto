69fa48065fb8cdc4f4dbfa7e9be33007e5747453
Jair <jair.guerreroaq@outlook.com>
2025-02-18 22:57:01 -0600

    add operador ternario

--- app.js
+++ app.js
@@ -2,18 +2,24 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraIntentos = 'intento';
+//let palabraIntentos = 'intento';
+let maxIntentos = 3;
+
 while (numeroUsuario != numeroSecreto) {
   numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
 
   console.log(numeroUsuario);
-    /*
+  /*
     Este codigo realiza
     la comparacion
     */
   if (numeroUsuario == numeroSecreto) {
     //Acertamos, fue verdadera la condicion
-    alert(`Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+    alert(
+      `Acertaste, el numero es: ${numeroUsuario}. Lo hiciste en ${intentos} ${
+        intentos == 1 ? "intento" : "intentos"
+      }`
+    );
   } else {
     if (numeroUsuario > numeroSecreto) {
       alert("El numero secreto es menor");
@@ -21,11 +27,16 @@ while (numeroUsuario != numeroSecreto) {
       alert("El numero secreto es mayor");
     }
     //incrementamos el contador cuando no acierta
+    //intentos = intentos + 1;
+    //intentos += 1;
     intentos++;
-    palabraIntentos = 'intentos';
-    if (intentos > 3) {
-      palabraIntentos = 'llegaste al limite de intentos';
-        break;}
+    //palabraIntentos = 'intentos';
+    if (intentos > maxIntentos) {
+      alert(
+        `llegaste al limite de ${maxIntentos} intentos`
+      );
+      break;
+    }
     //La condicion no se cumplio
     //alert('Lo siento, no acertaste el numero');
   }
