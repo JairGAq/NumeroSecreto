c87df626f9be1ffb389a04ffcaebddadd98db4e7
Jair <jair.guerreroaq@outlook.com>
2025-02-19 12:12:16 -0600

    add math para numero semi-random

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Variables
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
+//console.log(numeroSecreto);
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraIntentos = 'intento';
 let maxIntentos = 3;
 
+
 while (numeroUsuario != numeroSecreto) {
-  numeroUsuario = prompt("Me indicas un numero entre 1 y 10 por favor:");
+  numeroUsuario = parseInt(prompt("Me indicas un numero entre 1 y 10 por favor:"));
 
   console.log(numeroUsuario);
   /*
